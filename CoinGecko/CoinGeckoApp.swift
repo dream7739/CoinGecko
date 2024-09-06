ae8d7b57c61b1ef4fad3792620aebdcbd94a9de9
홍정민 <dream7739@naver.com>
2024-09-07 01:05:57 +0900

    Initial Commit

---
+++ CoinGecko/CoinGeckoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinGeckoApp.swift
+//  CoinGecko
+//
+//  Created by 홍정민 on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CoinGeckoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
